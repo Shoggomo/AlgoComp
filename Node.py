04cea7667a204d5cb3cbd12a9268fcc4c7598f04
Jakob Schönicke <jakob.schoenicke@arcor.de>
2017-01-18 15:51:28 +0100

    Added Node Requirements

--- Node.py
+++ Node.py
@@ -1,5 +1,9 @@
 from config import FieldType
 
+//Distanz zu Beginn bei Startknoten 0.
+//Methode für Knoten mit geringster Distanz unter den unbesuchten Knoten.
+//Methode für finalen Pfad. Rückwärts von Zielknoten aus über Vorgänger.
+
 class Node:
     def __init__(self, posX, posY, field_type, distance):
         self.posX = posX
